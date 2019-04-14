a87e2941f9edc31e6452a773f4ecb37a07cc3325
salmonzhg <salmonzhg@foxmail.com>
2019-04-14 18:36:42 +0800

    Simplified.

--- src/S98.java
+++ src/S98.java
@@ -3,8 +3,6 @@ public class S98 {
 
     public boolean isValidBST(TreeNode root) {
         if (root == null) return true;
-        if (root.left != null && root.left.val >= root.val) return false;
-        if (root.right != null && root.right.val <= root.val) return false;
         return isValid(root, Long.MAX_VALUE, Long.MIN_VALUE);
     }
 
@@ -25,7 +23,7 @@ public class S98 {
     public static void main(String[] args) {
         S98 tester = new S98();
 
-        TreeNode root = new TreeNode(-1);
+        TreeNode root = new TreeNode(2);
         root.left = new TreeNode(1);
         root.right = new TreeNode(4);
 
