e81434abf9df44f7bbefade491a9ab53d95d49e7
salmonzhg <salmonzhg@foxmail.com>
2019-04-18 18:09:09 +0800

    DP.

--- src/S72.java
+++ src/S72.java
@@ -2,38 +2,37 @@ public class S72 {
     // https://leetcode-cn.com/problems/edit-distance/
 
     public int minDistance(String word1, String word2) {
-        int length1 = word1.length();
-        int length2 = word2.length();
+        char[] w1 = word1.toCharArray();
+        char[] w2 = word2.toCharArray();
 
-        char[] w1s = word1.toCharArray();
-        char[] w2s = word2.toCharArray();
+        // state
+        int[][] distance = new int[w1.length + 1][w2.length + 1];
 
-        int[][] distance = new int[length1 + 1][length2 + 1];
-
-        for (int i = 0; i < length1 + 1; i++) {
-            distance[i][0] = i;
-        }
-
-        for (int i = 0; i < length2 + 1; i++) {
+        // init
+        for (int i = 0; i < distance[0].length; i++) {
             distance[0][i] = i;
         }
 
-        for (int i = 1; i < length1 + 1; i++) {
-            for (int j = 1; j < length2 + 1; j++) {
-                if (w1s[i - 1] == w2s[j - 1]) {
-                    distance[i][j] = distance[i-1][j-1];
+        for (int i = 0; i < distance.length; i++) {
+            distance[i][0] = i;
+        }
+
+        // function
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                if (w1[i - 1] == w2[j - 1]) {
+                    distance[i][j] = distance[i - 1][j - 1];
                 } else {
                     distance[i][j] = minOf(
-                            distance[i-1][j-1] + 1, // 替换
-                            distance[i][j-1] + 1, //
-                            distance[i-1][j] + 1
+                            distance[i - 1][j - 1] + 1,
+                            distance[i - 1][j] + 1,
+                            distance[i][j - 1] + 1
                     );
-
                 }
             }
         }
 
-        return distance[length1][length2];
+        return distance[word1.length()][word2.length()];
     }
 
     public int minOf(int n1, int n2, int n3) {
